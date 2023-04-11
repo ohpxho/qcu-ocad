5f3702ce426d2b8a80647846756e77dbcee1f408
ohpxho <lemuel.k.costuna.so@gmail.com>
2023-04-11 18:09:10 +0800

    prod

--- app/config/config.php
+++ app/config/config.php
@@ -4,16 +4,16 @@
 	
 	//db 
 	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', '');
-	define('DB_NAME', 'qcu_eservice');
+	define('DB_USER', 'u415113248_root');
+	define('DB_PASS', 'x7AP$$BF9^');
+	define('DB_NAME', 'u415113248_qcu_eservice');
 	define('DB_CHARSET', 'utf8mb4');
 
 	//paths
 	define('APPROOT', dirname(dirname(__FILE__)));
 	define('WEBROOT', dirname(dirname(dirname(__FILE__))));
-	define('PORT', $_SERVER['SERVER_PORT']);
-	define('URLROOT', 'http://localhost:'.PORT.'/qcu-ocad');
+	
+	define('URLROOT', 'http://qcu.ocad.com');
 	
 	//site
 	define('SITENAME', 'QCU OCAD');
