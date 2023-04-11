2651ae765153bbe2550fa43be34ca06cc1c85240
ohpxho <lemuel.k.costuna.so@gmail.com>
2023-04-11 18:38:16 +0800

    build css

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -8,6 +8,7 @@
 	<link rel="icon" href="<?php echo URLROOT;?>/public/assets/img/logo.png">
 	<link rel="stylesheet" href="<?php echo URLROOT;?>/public/css/style.css">
 	<link rel="stylesheet" href="<?php echo URLROOT;?>/public/css/dist/datatables.css" />
+	<link rel="stylesheet" href="<?php echo URLROOT;?>/public/css/dist/build.css" />
 	<!--<script src="https://cdn.tailwindcss.com"></script>-->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>	
 	<script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js" type="text/javascript"></script>
